0bd645fd0c945a38411820ad348d062fa27bfa6a
Ollie570 <oliveiramiguel142@gmail.com>
2022-09-25 20:43:03 -0300

    Ajustes finais

--- js/script.js
+++ js/script.js
@@ -14,10 +14,23 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
     
-    if (pipePosition <= 120) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 115) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png';
+        mario.style.width = '80px';
+        mario.style.marginLeft = '40px';
+
+        //Para o loop: 
+        clearInterval(loop);
     }
     
 }, 10);
